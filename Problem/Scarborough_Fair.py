60c254ac2f862424359fcce865f92b695ca1701c
shehab shaat <71198524+ShehabShaat@users.noreply.github.com>
2022-09-21 15:07:18 +0300

    update

--- Problem/Scarborough_Fair.py
+++ Problem/Scarborough_Fair.py
@@ -1,14 +1,7 @@
-def fun(n, string):
-    global s
-    for _ in range(n[1]):
-        s = list(map(str, input().split()))
-        # string. = string[int(s[0]):int(s[1])]
-
-    return string
-
-
-# (int(s[1]) - int(s[0])) + 1
 
 n = list(map(int, input().split()))
 string = input()
-print(fun(n, string))
+for _ in range(n[1]):
+    s = list(map(str, input().split()))
+    string = string[:int(s[0])-1]+string[int(s[0]) - 1:int(s[1])].replace(s[2], s[3])+string[int(s[1]):]
+print(string)
