567cab9abf28e84e83359dbc10d3c30f43c38eb0
shehab shaat <71198524+ShehabShaat@users.noreply.github.com>
2022-09-21 15:15:03 +0300

    update

--- Problem/Scarborough_Fair.py
+++ Problem/Scarborough_Fair.py
@@ -1,7 +1,15 @@
+# n = list(map(int, input().split()))
+# string = input()
+# for _ in range(n[1]):
+#     s = list(map(str, input().split()))
+#     string = string[:int(s[0]) - 1] + string[int(s[0]) - 1:int(s[1])].replace(s[2], s[3]) + string[int(s[1]):]
+# print(string)
 
-n = list(map(int, input().split()))
+# other solution
+
+n, m = map(int, input().split())
 string = input()
-for _ in range(n[1]):
-    s = list(map(str, input().split()))
-    string = string[:int(s[0])-1]+string[int(s[0]) - 1:int(s[1])].replace(s[2], s[3])+string[int(s[1]):]
+for i in range(m):
+    l, r, c1, c2 = (input().split())
+    string = string[:int(l) - 1] + string[int(l) - 1:int(r)].replace(c1, c2) + string[int(r):]
 print(string)
